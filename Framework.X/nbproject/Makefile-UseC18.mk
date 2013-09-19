fbe7f7c94426ec370cef9a53759ae65cfa7c1a60
Alex Mosolgo <amosolgo@gmail.com>
2013-09-18 20:57:07 -0400

    Commit all files which change when the project is simply opened in MPLAB X.

--- Framework.X/nbproject/Makefile-UseC18.mk
+++ Framework.X/nbproject/Makefile-UseC18.mk
@@ -8,14 +8,18 @@
 
 
 # Include project Makefile
+ifeq "${IGNORE_LOCAL}" "TRUE"
+# do not include local makefile. User is passing all local related variables already
+else
 include Makefile
 # Include makefile containing local settings
 ifeq "$(wildcard nbproject/Makefile-local-UseC18.mk)" "nbproject/Makefile-local-UseC18.mk"
 include nbproject/Makefile-local-UseC18.mk
 endif
+endif
 
 # Environment
-MKDIR=mkdir -p
+MKDIR=gnumkdir -p
 RM=rm -f 
 MV=mv 
 CP=cp 
@@ -62,7 +66,7 @@ LDLIBSOPTIONS=
 FIXDEPS=fixDeps
 
 .build-conf:  ${BUILD_SUBPROJECTS}
-	${MAKE}  -f nbproject/Makefile-UseC18.mk dist/${CND_CONF}/${IMAGE_TYPE}/Framework.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+	${MAKE} ${MAKE_OPTIONS} -f nbproject/Makefile-UseC18.mk dist/${CND_CONF}/${IMAGE_TYPE}/Framework.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
 
 MP_PROCESSOR_OPTION=18F26J50
 MP_PROCESSOR_OPTION_LD=18f26j50
@@ -79,63 +83,63 @@ ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 ${OBJECTDIR}/_ext/1360937237/interrupts.o: ../src/interrupts.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1360937237 
 	@${RM} ${OBJECTDIR}/_ext/1360937237/interrupts.o.d 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PICKIT2=1 -p$(MP_PROCESSOR_OPTION) -ms -oa- -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1360937237/interrupts.o   ../src/interrupts.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PICKIT2=1 -p$(MP_PROCESSOR_OPTION) -ms -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}\\..\\h  -fo ${OBJECTDIR}/_ext/1360937237/interrupts.o   ../src/interrupts.c 
 	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1360937237/interrupts.o 
 	@${FIXDEPS} "${OBJECTDIR}/_ext/1360937237/interrupts.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c18 
 	
 ${OBJECTDIR}/_ext/1360937237/main.o: ../src/main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1360937237 
 	@${RM} ${OBJECTDIR}/_ext/1360937237/main.o.d 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PICKIT2=1 -p$(MP_PROCESSOR_OPTION) -ms -oa- -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1360937237/main.o   ../src/main.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PICKIT2=1 -p$(MP_PROCESSOR_OPTION) -ms -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}\\..\\h  -fo ${OBJECTDIR}/_ext/1360937237/main.o   ../src/main.c 
 	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1360937237/main.o 
 	@${FIXDEPS} "${OBJECTDIR}/_ext/1360937237/main.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c18 
 	
 ${OBJECTDIR}/_ext/1360937237/messages.o: ../src/messages.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1360937237 
 	@${RM} ${OBJECTDIR}/_ext/1360937237/messages.o.d 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PICKIT2=1 -p$(MP_PROCESSOR_OPTION) -ms -oa- -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1360937237/messages.o   ../src/messages.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PICKIT2=1 -p$(MP_PROCESSOR_OPTION) -ms -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}\\..\\h  -fo ${OBJECTDIR}/_ext/1360937237/messages.o   ../src/messages.c 
 	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1360937237/messages.o 
 	@${FIXDEPS} "${OBJECTDIR}/_ext/1360937237/messages.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c18 
 	
 ${OBJECTDIR}/_ext/1360937237/my_i2c.o: ../src/my_i2c.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1360937237 
 	@${RM} ${OBJECTDIR}/_ext/1360937237/my_i2c.o.d 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PICKIT2=1 -p$(MP_PROCESSOR_OPTION) -ms -oa- -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1360937237/my_i2c.o   ../src/my_i2c.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PICKIT2=1 -p$(MP_PROCESSOR_OPTION) -ms -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}\\..\\h  -fo ${OBJECTDIR}/_ext/1360937237/my_i2c.o   ../src/my_i2c.c 
 	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1360937237/my_i2c.o 
 	@${FIXDEPS} "${OBJECTDIR}/_ext/1360937237/my_i2c.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c18 
 	
 ${OBJECTDIR}/_ext/1360937237/my_uart.o: ../src/my_uart.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1360937237 
 	@${RM} ${OBJECTDIR}/_ext/1360937237/my_uart.o.d 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PICKIT2=1 -p$(MP_PROCESSOR_OPTION) -ms -oa- -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1360937237/my_uart.o   ../src/my_uart.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PICKIT2=1 -p$(MP_PROCESSOR_OPTION) -ms -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}\\..\\h  -fo ${OBJECTDIR}/_ext/1360937237/my_uart.o   ../src/my_uart.c 
 	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1360937237/my_uart.o 
 	@${FIXDEPS} "${OBJECTDIR}/_ext/1360937237/my_uart.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c18 
 	
 ${OBJECTDIR}/_ext/1360937237/timer0_thread.o: ../src/timer0_thread.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1360937237 
 	@${RM} ${OBJECTDIR}/_ext/1360937237/timer0_thread.o.d 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PICKIT2=1 -p$(MP_PROCESSOR_OPTION) -ms -oa- -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1360937237/timer0_thread.o   ../src/timer0_thread.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PICKIT2=1 -p$(MP_PROCESSOR_OPTION) -ms -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}\\..\\h  -fo ${OBJECTDIR}/_ext/1360937237/timer0_thread.o   ../src/timer0_thread.c 
 	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1360937237/timer0_thread.o 
 	@${FIXDEPS} "${OBJECTDIR}/_ext/1360937237/timer0_thread.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c18 
 	
 ${OBJECTDIR}/_ext/1360937237/timer1_thread.o: ../src/timer1_thread.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1360937237 
 	@${RM} ${OBJECTDIR}/_ext/1360937237/timer1_thread.o.d 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PICKIT2=1 -p$(MP_PROCESSOR_OPTION) -ms -oa- -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1360937237/timer1_thread.o   ../src/timer1_thread.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PICKIT2=1 -p$(MP_PROCESSOR_OPTION) -ms -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}\\..\\h  -fo ${OBJECTDIR}/_ext/1360937237/timer1_thread.o   ../src/timer1_thread.c 
 	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1360937237/timer1_thread.o 
 	@${FIXDEPS} "${OBJECTDIR}/_ext/1360937237/timer1_thread.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c18 
 	
 ${OBJECTDIR}/_ext/1360937237/uart_thread.o: ../src/uart_thread.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1360937237 
 	@${RM} ${OBJECTDIR}/_ext/1360937237/uart_thread.o.d 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PICKIT2=1 -p$(MP_PROCESSOR_OPTION) -ms -oa- -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1360937237/uart_thread.o   ../src/uart_thread.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PICKIT2=1 -p$(MP_PROCESSOR_OPTION) -ms -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}\\..\\h  -fo ${OBJECTDIR}/_ext/1360937237/uart_thread.o   ../src/uart_thread.c 
 	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1360937237/uart_thread.o 
 	@${FIXDEPS} "${OBJECTDIR}/_ext/1360937237/uart_thread.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c18 
 	
 ${OBJECTDIR}/_ext/1360937237/user_interrupts.o: ../src/user_interrupts.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1360937237 
 	@${RM} ${OBJECTDIR}/_ext/1360937237/user_interrupts.o.d 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PICKIT2=1 -p$(MP_PROCESSOR_OPTION) -ms -oa- -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1360937237/user_interrupts.o   ../src/user_interrupts.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PICKIT2=1 -p$(MP_PROCESSOR_OPTION) -ms -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}\\..\\h  -fo ${OBJECTDIR}/_ext/1360937237/user_interrupts.o   ../src/user_interrupts.c 
 	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1360937237/user_interrupts.o 
 	@${FIXDEPS} "${OBJECTDIR}/_ext/1360937237/user_interrupts.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c18 
 	
@@ -143,63 +147,63 @@ else
 ${OBJECTDIR}/_ext/1360937237/interrupts.o: ../src/interrupts.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1360937237 
 	@${RM} ${OBJECTDIR}/_ext/1360937237/interrupts.o.d 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION) -ms -oa- -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1360937237/interrupts.o   ../src/interrupts.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION) -ms -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}\\..\\h  -fo ${OBJECTDIR}/_ext/1360937237/interrupts.o   ../src/interrupts.c 
 	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1360937237/interrupts.o 
 	@${FIXDEPS} "${OBJECTDIR}/_ext/1360937237/interrupts.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c18 
 	
 ${OBJECTDIR}/_ext/1360937237/main.o: ../src/main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1360937237 
 	@${RM} ${OBJECTDIR}/_ext/1360937237/main.o.d 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION) -ms -oa- -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1360937237/main.o   ../src/main.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION) -ms -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}\\..\\h  -fo ${OBJECTDIR}/_ext/1360937237/main.o   ../src/main.c 
 	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1360937237/main.o 
 	@${FIXDEPS} "${OBJECTDIR}/_ext/1360937237/main.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c18 
 	
 ${OBJECTDIR}/_ext/1360937237/messages.o: ../src/messages.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1360937237 
 	@${RM} ${OBJECTDIR}/_ext/1360937237/messages.o.d 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION) -ms -oa- -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1360937237/messages.o   ../src/messages.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION) -ms -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}\\..\\h  -fo ${OBJECTDIR}/_ext/1360937237/messages.o   ../src/messages.c 
 	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1360937237/messages.o 
 	@${FIXDEPS} "${OBJECTDIR}/_ext/1360937237/messages.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c18 
 	
 ${OBJECTDIR}/_ext/1360937237/my_i2c.o: ../src/my_i2c.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1360937237 
 	@${RM} ${OBJECTDIR}/_ext/1360937237/my_i2c.o.d 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION) -ms -oa- -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1360937237/my_i2c.o   ../src/my_i2c.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION) -ms -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}\\..\\h  -fo ${OBJECTDIR}/_ext/1360937237/my_i2c.o   ../src/my_i2c.c 
 	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1360937237/my_i2c.o 
 	@${FIXDEPS} "${OBJECTDIR}/_ext/1360937237/my_i2c.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c18 
 	
 ${OBJECTDIR}/_ext/1360937237/my_uart.o: ../src/my_uart.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1360937237 
 	@${RM} ${OBJECTDIR}/_ext/1360937237/my_uart.o.d 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION) -ms -oa- -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1360937237/my_uart.o   ../src/my_uart.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION) -ms -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}\\..\\h  -fo ${OBJECTDIR}/_ext/1360937237/my_uart.o   ../src/my_uart.c 
 	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1360937237/my_uart.o 
 	@${FIXDEPS} "${OBJECTDIR}/_ext/1360937237/my_uart.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c18 
 	
 ${OBJECTDIR}/_ext/1360937237/timer0_thread.o: ../src/timer0_thread.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1360937237 
 	@${RM} ${OBJECTDIR}/_ext/1360937237/timer0_thread.o.d 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION) -ms -oa- -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1360937237/timer0_thread.o   ../src/timer0_thread.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION) -ms -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}\\..\\h  -fo ${OBJECTDIR}/_ext/1360937237/timer0_thread.o   ../src/timer0_thread.c 
 	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1360937237/timer0_thread.o 
 	@${FIXDEPS} "${OBJECTDIR}/_ext/1360937237/timer0_thread.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c18 
 	
 ${OBJECTDIR}/_ext/1360937237/timer1_thread.o: ../src/timer1_thread.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1360937237 
 	@${RM} ${OBJECTDIR}/_ext/1360937237/timer1_thread.o.d 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION) -ms -oa- -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1360937237/timer1_thread.o   ../src/timer1_thread.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION) -ms -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}\\..\\h  -fo ${OBJECTDIR}/_ext/1360937237/timer1_thread.o   ../src/timer1_thread.c 
 	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1360937237/timer1_thread.o 
 	@${FIXDEPS} "${OBJECTDIR}/_ext/1360937237/timer1_thread.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c18 
 	
 ${OBJECTDIR}/_ext/1360937237/uart_thread.o: ../src/uart_thread.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1360937237 
 	@${RM} ${OBJECTDIR}/_ext/1360937237/uart_thread.o.d 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION) -ms -oa- -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1360937237/uart_thread.o   ../src/uart_thread.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION) -ms -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}\\..\\h  -fo ${OBJECTDIR}/_ext/1360937237/uart_thread.o   ../src/uart_thread.c 
 	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1360937237/uart_thread.o 
 	@${FIXDEPS} "${OBJECTDIR}/_ext/1360937237/uart_thread.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c18 
 	
 ${OBJECTDIR}/_ext/1360937237/user_interrupts.o: ../src/user_interrupts.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1360937237 
 	@${RM} ${OBJECTDIR}/_ext/1360937237/user_interrupts.o.d 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION) -ms -oa- -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1360937237/user_interrupts.o   ../src/user_interrupts.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION) -ms -Ou- -Ot- -Ob- -Op- -Or- -Od- -Opa- --extended -sa -I ${MP_CC_DIR}\\..\\h  -fo ${OBJECTDIR}/_ext/1360937237/user_interrupts.o   ../src/user_interrupts.c 
 	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1360937237/user_interrupts.o 
 	@${FIXDEPS} "${OBJECTDIR}/_ext/1360937237/user_interrupts.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c18 
 	
@@ -208,15 +212,15 @@ endif
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: link
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-dist/${CND_CONF}/${IMAGE_TYPE}/Framework.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
+dist/${CND_CONF}/${IMAGE_TYPE}/Framework.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_LD} $(MP_EXTRA_LD_PRE)   -p$(MP_PROCESSOR_OPTION_LD)  -w -x -u_DEBUG -w -u_EXTENDEDMODE -g -z__MPLAB_BUILD=1  -u_CRUNTIME -z__MPLAB_DEBUG=1 -z__MPLAB_DEBUGGER_PICKIT2=1 $(MP_LINKER_DEBUG_OPTION) -l ${MP_CC_DIR}/../lib  -o dist/${CND_CONF}/${IMAGE_TYPE}/Framework.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}   
+	${MP_LD} $(MP_EXTRA_LD_PRE)   -p$(MP_PROCESSOR_OPTION_LD)  -w -x -u_DEBUG -w -u_EXTENDEDMODE -g -z__MPLAB_BUILD=1  -u_CRUNTIME -z__MPLAB_DEBUG=1 -z__MPLAB_DEBUGGER_PICKIT2=1 $(MP_LINKER_DEBUG_OPTION) -l ${MP_CC_DIR}\\..\\lib  -o dist/${CND_CONF}/${IMAGE_TYPE}/Framework.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}   
 else
 dist/${CND_CONF}/${IMAGE_TYPE}/Framework.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_LD} $(MP_EXTRA_LD_PRE)   -p$(MP_PROCESSOR_OPTION_LD)  -w  -w -u_EXTENDEDMODE -g -z__MPLAB_BUILD=1  -u_CRUNTIME -l ${MP_CC_DIR}/../lib  -o dist/${CND_CONF}/${IMAGE_TYPE}/Framework.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}   
+	${MP_LD} $(MP_EXTRA_LD_PRE)   -p$(MP_PROCESSOR_OPTION_LD)  -w  -w -u_EXTENDEDMODE -g -z__MPLAB_BUILD=1  -u_CRUNTIME -l ${MP_CC_DIR}\\..\\lib  -o dist/${CND_CONF}/${IMAGE_TYPE}/Framework.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}   
 	@echo Normalizing hex file
-	@"/Applications/microchip/mplabx/mplab_ide.app/Contents/Resources/mplab_ide/mplab_ide/modules/../../bin/hexmate" --edf="/Applications/microchip/mplabx/mplab_ide.app/Contents/Resources/mplab_ide/mplab_ide/modules/../../bin/en_msgs.txt" dist/${CND_CONF}/${IMAGE_TYPE}/Framework.X.${IMAGE_TYPE}.hex -odist/${CND_CONF}/${IMAGE_TYPE}/Framework.X.${IMAGE_TYPE}.hex
+	@"C:/Program Files (x86)/Microchip/MPLABX/mplab_ide/mplab_ide/modules/../../bin/hexmate" --edf="C:/Program Files (x86)/Microchip/MPLABX/mplab_ide/mplab_ide/modules/../../bin/en_msgs.txt" dist/${CND_CONF}/${IMAGE_TYPE}/Framework.X.${IMAGE_TYPE}.hex -odist/${CND_CONF}/${IMAGE_TYPE}/Framework.X.${IMAGE_TYPE}.hex
 
 endif
 
@@ -236,7 +240,7 @@ endif
 # Enable dependency checking
 .dep.inc: .depcheck-impl
 
-DEPFILES=$(shell "${PATH_TO_IDE_BIN}"mplabwildcard ${POSSIBLE_DEPFILES})
+DEPFILES=$(shell mplabwildcard ${POSSIBLE_DEPFILES})
 ifneq (${DEPFILES},)
 include ${DEPFILES}
 endif
