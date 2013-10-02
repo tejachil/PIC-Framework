52894e3bfa492203ba2d829867ad2ff8073cd219
Alex Mosolgo <amosolgo@gmail.com>
2013-10-01 22:10:08 -0400

    Changed I2C status to use an enumerated type instead of #defines.

--- src/my_i2c.h
+++ src/my_i2c.h
@@ -11,23 +11,34 @@
 #endif
 
 #define MAXI2CBUF MSGLEN
+
 typedef struct __i2c_comm {
-	unsigned char buffer[MAXI2CBUF];
-	unsigned char	buflen;
-	unsigned char	event_count;
-	unsigned char	status;
-	unsigned char	error_code;
-	unsigned char	error_count;
-	unsigned char outbuffer[MAXI2CBUF];
-	unsigned char outbuflen;
-	unsigned char outbufind;
-	unsigned char slave_addr;
+    unsigned char buffer[MAXI2CBUF];
+    unsigned char buflen;
+    unsigned char event_count;
+#ifdef I2C_SLAVE
+    I2CSlaveStatus status;
+#else
+    I2CMasterStatus status;
+#endif
+    unsigned char error_code;
+    unsigned char error_count;
+    unsigned char outbuffer[MAXI2CBUF];
+    unsigned char outbuflen;
+    unsigned char outbufind;
+    unsigned char slave_addr;
 } i2c_comm;
 
-#define I2C_IDLE 0x5
-#define I2C_STARTED 0x6
-#define	I2C_RCV_DATA 0x7
-#define I2C_SLAVE_SEND 0x8
+typedef enum {
+    I2C_IDLE = 0x5,
+    I2C_STARTED = 0x6,
+    I2C_RCV_DATA = 0x7,
+    I2C_SLAVE_SEND = 0x8
+} I2CSlaveStatus;
+
+typedef enum {
+    I2C_IDLE
+} I2CMasterStatus;
 
 #define I2C_ERR_THRESHOLD 1
 #define I2C_ERR_OVERRUN 0x4
@@ -38,10 +49,10 @@ typedef struct __i2c_comm {
 
 void init_i2c(i2c_comm *);
 void i2c_int_handler(void);
-void start_i2c_slave_reply(unsigned char,unsigned char *);
+void start_i2c_slave_reply(unsigned char, unsigned char *);
 void i2c_configure_slave(unsigned char);
 void i2c_configure_master(unsigned char);
-unsigned char i2c_master_send(unsigned char,unsigned char *);
+unsigned char i2c_master_send(unsigned char, unsigned char *);
 unsigned char i2c_master_recv(unsigned char);
 
 #endif
