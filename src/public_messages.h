7828eaa2c88374f8f9ae321715d7b87c617030f2
Alex Mosolgo <amosolgo@gmail.com>
2013-10-06 19:38:43 -0400

    Added public_messages.h to define message types, message format, etc.

---
+++ src/public_messages.h
@@ -0,0 +1,51 @@
+#ifndef PUBLIC_MESSAGES_H
+#define	PUBLIC_MESSAGES_H
+
+/** Public message type values. */
+typedef enum {
+    /** Distance reading for a given sensor. */
+    PUB_MSG_T_SENS_DIST = 0,
+    /** Instruction to move rover forward specified distance. */
+    PUB_MSG_T_MOV_CMD,
+    /** Instruction to rotate rover specified direction and angle. */
+    PUB_MSG_T_TURN_CMD,
+    /** Angle of wall to side relative to rover. */
+    PUB_MSG_T_SIDE_ANGLE,
+    /** Angle of wall in front relative to rover. */
+    PUB_MSG_T_FRONT_ANGLE,
+    /** Information about the current enclosure. */
+    PUB_MSG_T_ENC_INFO,
+    /** Position of rover. */
+    PUB_MSG_T_ROV_POS,
+    /** Notification of new enclosure corner. */
+    PUB_MSG_T_NEW_CORNER,
+    /** Notification for webserver of new enclosure corner. */
+    PUB_MSG_T_CORNER_WEB,
+
+    // ADD ANY NEW MESSAGE TYPES ABOVE THIS LINE
+    // So that NUM_PUB_MSG_T will be correct.
+
+    /** Number of defined public message types. */
+    NUM_PUB_MSG_T
+} public_message_type_t;
+
+/**
+ * Max number of bytes of data allowed to be attached to a public message.
+ * Defined as the maximum internal message length minus 3 bytes for the message
+ * type, count, and length.
+ */
+#define PUB_MSG_MAX_DATA (10 - 3)
+
+/** Minimum size of a public message (with data length 0). */
+#define PUB_MSG_MIN_SIZE (3)
+
+/** Structure of public messages. */
+typedef struct {
+    public_message_type_t message_type;
+    unsigned char message_count;
+    unsigned char data_length;
+    unsigned char data[PUB_MSG_MAX_DATA];
+} public_message_t;
+
+#endif	/* PUBLIC_MESSAGES_H */
+
