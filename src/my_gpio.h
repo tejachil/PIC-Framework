e7381f98c5def70f442ebeba408b087ce9e3d2b7
Alex Mosolgo <amosolgo@gmail.com>
2013-10-29 17:40:33 -0400

    Removed UART Rx timer GPIO definitions for MOTOR_PIC.

--- src/my_gpio.h
+++ src/my_gpio.h
@@ -6,14 +6,14 @@
 #ifndef MY_GPIO_H
 #define	MY_GPIO_H
 
-#if defined(MASTER_PIC) || defined(MOTOR_PIC)
+#ifdef MASTER_PIC
 /** Pin toggled on UART Rx timeout. */
 #define TOGGLE_UART_RX_TIMEOUT_PIN() (LATBbits.LATB0 ^= 1)
 /** Pin toggled when the UART Rx timeout is stoppped. */
 #define TOGGLE_UART_RX_TIMEOUT_STOP_PIN() (LATBbits.LATB1 ^= 1)
 /** Pin toggled when the UART Rx timeout is started. */
 #define TOGGLE_UART_RX_TIMEOUT_START_PIN() (LATBbits.LATB2 ^= 1)
-#endif // MASTER_PIC || MOTOR_PIC
+#endif // MASTER_PIC
 
 void gpio_init_portb_output(void);
 void gpio_write_portb(const unsigned char val);
