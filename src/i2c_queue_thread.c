d85029edcdbc0783df51de10adef7d2805449d91
Alex Mosolgo <amosolgo@gmail.com>
2013-11-04 22:23:14 -0500

    Fixed message size calculation in i2c_queue_lthread().

--- src/i2c_queue_thread.c
+++ src/i2c_queue_thread.c
@@ -12,17 +12,16 @@ void i2c_queue_lthread(int msgtype, int length, unsigned char *msgbuffer) {
         const unsigned char type = msg->message_type;
         const unsigned char *raw_msg = msg->raw_message_bytes;
 
+        // Determine the total size for this message
+        const unsigned char msg_total_size = public_message_data_size[type] + PUB_MSG_MIN_SIZE;
+
         // Check the message type to determine where to send the message and
         // send it along.
         switch (type) {
             case PUB_MSG_T_SENS_DIST:
             {
-                // Determine the expected size for the message when it is received
-                // from the I2C slave device.
-                const unsigned char exp_msg_size = public_message_data_size[type] + PUB_MSG_MIN_SIZE;
-
                 // Read the full message from the slave
-                i2c_master_read(SENSOR_PIC_ADDR, type, exp_msg_size);
+                i2c_master_read(SENSOR_PIC_ADDR, type, msg_total_size);
 
                 break;
             } // End requests for Proximity Sensors PIC
@@ -32,7 +31,7 @@ void i2c_queue_lthread(int msgtype, int length, unsigned char *msgbuffer) {
             case PUB_MSG_T_FIX_CMD:
             {
                 // Forward the full message to the slave
-                i2c_master_write(MOTOR_PIC_ADDR, raw_msg, length);
+                i2c_master_write(MOTOR_PIC_ADDR, raw_msg, msg_total_size);
 
                 break;
             } // End commands for Motor Controller PIC
